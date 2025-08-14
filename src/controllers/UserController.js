3e1df07697e178ed2c7bdf0885215d4d71f537c1
bdvduc <bdvduc@rsupport.com>
2025-08-14 22:34:31 +0700

    API login and update user

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -2,7 +2,6 @@ const UserService = require("../services/UserService");
 
 const createUser = async (req, res) => {
   try {
-    console.log(req.body);
     const { name, email, password, confirmPassword, phone } = req.body;
     const reg = /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
     const isCheckEmail = reg.test(email);
@@ -33,4 +32,55 @@ const createUser = async (req, res) => {
   }
 };
 
-module.exports = { createUser };
+const loginUser = async (req, res) => {
+  try {
+    const { name, email, password, confirmPassword, phone } = req.body;
+    const reg = /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+    const isCheckEmail = reg.test(email);
+
+    if (!name || !email || !password || !confirmPassword || !phone) {
+      return res.status(200).json({
+        status: "ERROR",
+        message: "Required",
+      });
+    } else if (!isCheckEmail) {
+      return res.status(200).json({
+        status: "ERROR",
+        message: "Please check your email",
+      });
+    } else if (password !== confirmPassword) {
+      return res.status(200).json({
+        status: "ERROR",
+        message: "Please check your password",
+      });
+    }
+
+    const response = await UserService.loginUser(req.body);
+    return res.status(200).json(response);
+  } catch (e) {
+    return res.status(404).json({
+      message: e,
+    });
+  }
+};
+
+const updateUser = async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const data = req.body;
+    if (!userId) {
+      return res.status(200).json({
+        status: "ERROR",
+        message: "UserID is required",
+      });
+    }
+    const response = await UserService.updateUser(userId, data);
+    return res.status(200).json(response);
+  } catch (e) {
+    return res.status(404).json({
+      message: e,
+    });
+  }
+};
+
+module.exports = { createUser, loginUser, updateUser };
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,8 +1,10 @@
 const User = require("../models/UserModal");
+const bcrypt = require("bcrypt");
+const { generalAccessToken } = require("./JwtService");
 
 const createUser = (newUser) => {
   return new Promise(async (resolve, reject) => {
-    const { name, email, password, confirmPassword, phone } = newUser;
+    const { name, email, password, phone } = newUser;
     try {
       const checkUser = await User.findOne({
         email,
@@ -13,11 +15,11 @@ const createUser = (newUser) => {
           message: "Email already exists",
         });
       } else {
+        const hashPassword = bcrypt.hashSync(password, 10);
         const createUser = await User.create({
           name,
           email,
-          password,
-          confirmPassword,
+          password: hashPassword,
           phone,
         });
         if (createUser) {
@@ -33,5 +35,66 @@ const createUser = (newUser) => {
     }
   });
 };
+const loginUser = (userLogin) => {
+  return new Promise(async (resolve, reject) => {
+    const { name, email, password, confirmPassword, phone } = userLogin;
+    try {
+      const checkUser = await User.findOne({
+        email,
+      });
+      if (checkUser === null) {
+        resolve({
+          status: "OK",
+          message: "User is not defined",
+        });
+      }
+      const comparePassword = bcrypt.compareSync(password, checkUser.password);
+      if (!comparePassword) {
+        resolve({
+          status: "OK",
+          message: "The user or password is incorrect",
+        });
+      }
+      const access_token = await generalAccessToken({
+        id: checkUser._id,
+        isAdmin: checkUser.isAdmin,
+      });
+      const refresh_token = await generalAccessToken({
+        id: checkUser._id,
+        isAdmin: checkUser.isAdmin,
+      });
+      resolve({
+        status: "OK",
+        message: "SUCCESS",
+        access_token,
+        refresh_token,
+      });
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+const updateUser = (id, data) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const checkUser = await User.findOne({ _id: id });
+      if (checkUser === null) {
+        resolve({
+          status: "OK",
+          message: "User is not defined",
+        });
+      }
 
-module.exports = { createUser };
+      const updateUser = await User.findByIdAndUpdate(id, data, { new: true });
+      resolve({
+        status: "OK",
+        message: "SUCCESS",
+        data: updateUser,
+      });
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+module.exports = { createUser, loginUser, updateUser };
